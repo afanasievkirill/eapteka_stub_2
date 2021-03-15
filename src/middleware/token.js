0266fec802bcf439a234d64e30e8a906f46b756a
k.afanasev <afanasev.kirill@gmail.com>
2021-03-15 11:36:38 +0300

    Update exception

--- src/middleware/token.js
+++ src/middleware/token.js
@@ -2,10 +2,10 @@ module.exports = function (req, res, next) {
     const token = req.query.api_key
     console.log(token)
     if(!token){
-        return res.status(401).json({msg: 'No token, authorization denied'})
+        return res.status(401).json({msg: 'No api key, authorization denied'})
     }
     if(token !== 'test_api'){
-        return res.status(401).json({msg: 'Token is not valid'})
+        return res.status(401).json({msg: 'Api key is not valid'})
     }
     next()
 }
