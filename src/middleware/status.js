60570f74c18760094397e8c7b64e2bdaecd36c6a
k.afanasev <afanasev.kirill@gmail.com>
2021-03-17 18:13:39 +0300

    Add GUID check.

--- src/middleware/status.js
+++ src/middleware/status.js
@@ -1,5 +1,7 @@
 const {body, validationResult} = require('express-validator')
 
+const {isGuid} = require('check-guid')
+
 exports.validate = [
     body('*.order_id').not().isEmpty(),
     body('*.status', 'Status is empty').not().isEmpty(),
@@ -19,6 +21,14 @@ exports.validate = [
         const errors = validationResult(req);
         if (!errors.isEmpty())
             return res.status(422).json({errors: errors.array()});
+        const json_entities = req.body
+        json_entities.forEach( function(entity) {
+            const isValueGuid  =  isGuid(entity.order_id)
+            if(!isValueGuid){
+                console.log('bad_guid')
+                return res.status(400).json({msg: 'Order_id has not valid guid'})
+            }
+        })
         next();
     }
 ]
